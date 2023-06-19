645254f69b1f014bed726d3d4d7b9e339d051363
billy chorey <billychorey@gmail.com>
2023-06-19 17:08:13 -0400

    adds loop exercises

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solutions in this file
+const writeCards = (arr,event) => {
+    const messages = []
+    for(let i = 0; i < arr.length;i++) {
+        
+           const greetingMessage = `Thank you, ${arr[i]}, for the wonderful ${event} gift!`
+           messages.push(greetingMessage)
+    }
+    return messages
+
+}
+          
+
+console.log(writeCards(["Charlie", "Samip", "Ali"], "birthday"))
+
+function countDown() {
+    let countdown = 10;
+    while (countdown >= 0) {
+        console.log(countdown--);
+    }
+}
+countDown();
